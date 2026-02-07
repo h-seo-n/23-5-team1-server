cfc6b78ce98f1189a942e17918962e03587d31e9
aystoe <hye__n@snu.ac.kr>
2026-02-07 14:05:55 +0900

    webconfig

--- hangsha/src/main/kotlin/com/team1/hangsha/config/WebConfig.kt
+++ hangsha/src/main/kotlin/com/team1/hangsha/config/WebConfig.kt
@@ -1,15 +1,24 @@
 package com.team1.hangsha.config
 
 import com.team1.hangsha.user.UserArgumentResolver
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
     private val userArgumentResolver: UserArgumentResolver,
+    @Value("\${upload.dir}") private val uploadDir: String
 ) : WebMvcConfigurer {
+
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(userArgumentResolver)
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/static/**")
+            .addResourceLocations("file:$uploadDir/")
     }
 }
